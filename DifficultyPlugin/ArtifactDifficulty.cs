99d0533872f4c7b4597be557ac7e7cf6af6ae6e7
QWolfPlaY <st30943@gmail.com>
2022-11-05 23:15:27 +0100

    Initial commit

---
+++ DifficultyPlugin/ArtifactDifficulty.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArtifactDifficulty {
+    
+    internal class ArtifactDifficulty : DiffTemplate {
+
+        internal static ArtifactDifficulty def;
+        
+        public ArtifactDifficulty() {
+
+            Name = "Artifact";
+            BaseToken = "ARTIFACT_DIFFICULTY";
+            //rgb(55%,45%,73%)
+            Color = new Color(0.55f, 0.45f, 0.73f);
+            ScalingValue = 4f;
+            HealthRegenMod = 0.0f;
+            Tag = "ART";
+            IconPath = "artifact-" + DifficultyPlugin.
+            
+        }
+}
