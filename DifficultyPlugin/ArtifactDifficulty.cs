a67db13885bbe7a4de3f38afe55c4ca6d9c5ab78
QWolfPlaY <st30943@gmail.com>
2022-11-06 19:11:46 +0100

    r2api my beloved

--- DifficultyPlugin/ArtifactDifficulty.cs
+++ DifficultyPlugin/ArtifactDifficulty.cs
@@ -14,9 +14,37 @@ namespace ArtifactDifficulty {
             //rgb(55%,45%,73%)
             Color = new Color(0.55f, 0.45f, 0.73f);
             ScalingValue = 4f;
-            HealthRegenMod = 0.0f;
+            HealthRegenMod = -0.4f;
             Tag = "ART";
-            IconPath = "artifact-" + DifficultyPlugin.
+            IconPath = "artifact-" + DifficultyPlugin.artifactArtist.Value + ".png";
+            StartMessages = new string[] { "Start praying for your CPU", "Welcome to my Powerpoint presentation", "Say goodbye to your framerate", "Your eyes will hurt", "Better call yopur doctor in case of epilepsy" };
+            MonsterRegenMod = 0.0f;
+            EliteModifier = 0.65f;
+            Description = buildDescription();
+        }
+
+        public override Dictionary<string, string> Language => ArtifactDictionray.DictionaryToUse;
+
+        public override void ApplyHooks()
+        {
             
+        }
+        
+        public override void UndoHooks()
+        {
+            
+        }
+
+        private string buildDescription()
+        {
+            string desc = "For those who want to enjoy <style=cArtifact>artifact of sacrifice</style>.\n";
+            desc = string.Join("\n", desc, $">Difficulty Scaling: <style=cDeath>{ScalingValue * 50 - 100}%</style>",
+                $">Player Health Regen: <style=cHealing>{(int)(HealthRegenMod * 100)}%</style>",
+                $">Elites: <style=cShrine>35%</style> Cheaper",
+                $">LAG: <style=cArtifact>Guaranteed</style>",
+                $">CPU USAGE: <style=cMono>100%</style>"
+                );
+            desc += "</style>";
+            return desc;
         }
 }
